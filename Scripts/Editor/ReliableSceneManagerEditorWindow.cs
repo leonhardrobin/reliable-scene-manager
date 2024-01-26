a8229dabea12ffd036b8d30dd6925f222ee4ba55
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2024-01-26 17:23:11 +0100

    Add checks if in editor for editor scripts

--- Scripts/Editor/ReliableSceneManagerEditorWindow.cs
+++ Scripts/Editor/ReliableSceneManagerEditorWindow.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -180,3 +181,4 @@ namespace LRS.SceneManagement.Editor
         }
     }
 }
+#endif
--- Scripts/Editor/SceneReferencePropertyDrawer.cs
+++ Scripts/Editor/SceneReferencePropertyDrawer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -77,3 +78,4 @@ namespace LRS.SceneManagement.Editor
         }
     }
 }
+#endif

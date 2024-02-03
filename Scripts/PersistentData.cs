98a1ded48bb652ea7e1d544892d47b9e4baaaa8c
Leonhard Schnaitl <leonhard.schnaitl@gmail.com>
2024-02-03 10:45:11 +0100

    Add UnityMessageCallback Script

--- Scripts/PersistentData.cs
+++ Scripts/PersistentData.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace LRS.SceneManagement
 {
@@ -28,18 +29,66 @@ namespace LRS.SceneManagement
             }
         }
 
-        private readonly SerializableDictionary<string, object> _data = new();
-        
-        public static void Set<T>(string key, T value) => Instance._data[key] = value;
+        [SerializeField, HideInInspector] private SerializableDictionary<string, Object> objects = new();
+
+        [SerializeField, HideInInspector] private SerializableDictionary<string, object> data = new();
+
+        public static void Set<T>(string key, T value)
+        {
+            bool isObject = value is Object;
+
+            if (isObject)
+            {
+                Instance.objects[key] = value as Object;
+
+                switch (value)
+                {
+                    case GameObject go:
+                        var callbacks = go.GetOrAdd<UnityMessageCallbacks>();
+                        callbacks.onDestroy += obj =>
+                        {
+                            Instance.objects[key] = Instantiate(go);
+                        };
+                        break;
+                    case Component component:
+                        break;
+                }
+            }
+            else
+            {
+                Instance.data[key] = value;
+            }
+        }
+
+        public static T GetObj<T>(string key) where T : Object
+        {
+            if (Instance.objects.ContainsKey(key))
+            {
+                return Instance.objects[key] as T;
+            }
+
+            return null;
+        }
 
         public static T Get<T>(string key)
         {
-            if (Instance._data.ContainsKey(key))
+            if (Instance.data.ContainsKey(key))
             {
-                return (T) Instance._data[key];
+                return (T) Instance.data[key];
             }
 
             return default;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static T GetOrAdd<T>(this GameObject gameObject) where T : Component
+        {
+            T component = gameObject.GetComponent<T>();
+            if (!component) component = gameObject.AddComponent<T>();
+
+            return component;
         }
     }
 }
---
+++ Scripts/UnityMessageCallbacks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace LRS.SceneManagement
+{
+    internal class UnityMessageCallbacks : MonoBehaviour
+    {
+        public Action onEnable;
+        public Action onAwake;
+        public Action onStart;
+        public Action onUpdate;
+        public Action<GameObject> onDestroy;
+        public Action onDisable;
+
+        private void OnEnable() => onEnable?.Invoke();
+
+        private void Awake() => onAwake?.Invoke();
+
+        private void Start() => onStart?.Invoke();
+
+        private void Update() => onUpdate?.Invoke();
+
+        private void OnDestroy() => onDestroy?.Invoke(gameObject);
+
+        private void OnDisable() => onDisable?.Invoke();
+    }
+}
